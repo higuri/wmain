faf3f7546db32f91d87070dd4021a1995ba0ed84
higuri <35358263+higuri@users.noreply.github.com>
2018-02-01 22:27:12 +0900

    Add -L option

--- main.c
+++ main.c
@@ -6,6 +6,16 @@
 #include <wchar.h>
 #include <locale.h>
 
+// array_remove()
+static int array_remove(void** buf, int i, int c) {
+    for (int j = i; j < c; j++) {
+        if (j+1 < c) {
+            buf[j] = buf[j+1];
+        }
+    }
+    return (c-1);
+}
+
 // free_wargs()
 static void free_wargs(
     int wargc, wchar_t** wargs) {
@@ -25,13 +35,17 @@ static wchar_t** alloc_wargs(
     wchar_t** wargs =
         (wchar_t**)calloc(argc, sizeof(wchar_t*));
     for (int i = 0; i < argc; i++) {
-        char* ncs = args[i];
-        int maxlen = strlen(ncs);
-        wargs[i] = (wchar_t*)
-            calloc(maxlen+1, sizeof(wchar_t));
-        size_t ret =
-            mbstowcs(wargs[i], ncs, maxlen);
-        if ((int)ret < 0) {
+        char* mbs = args[i];
+        // Get the length for wide characters.
+        size_t wcslen = mbstowcs(NULL, mbs, 0);
+        if (wcslen == (size_t)-1) {
+            free_wargs(argc, wargs);
+            return NULL;
+        }
+        // Allocate mbs to wcs.
+        wargs[i] = (wchar_t*)calloc(wcslen+1, sizeof(wchar_t));
+        size_t ret = mbstowcs(wargs[i], mbs, wcslen+1);
+        if (ret == (size_t)-1) {
             free_wargs(argc, wargs);
             return NULL;
         }
@@ -39,16 +53,46 @@ static wchar_t** alloc_wargs(
     return wargs;
 }
 
+// alloc_wargs_with_locale()
+static wchar_t** alloc_wargs_with_locale(
+    int argc, char* args[], char* localeID) {
+    char* old_locale = (char*)strdup(setlocale(LC_ALL, NULL));
+    if (old_locale == NULL) {
+        wprintf(L"Error: Getting current locale failed.");
+        return NULL;
+    }
+    if (setlocale(LC_ALL, localeID) == NULL) {
+        wprintf(L"Error: Invalid locale ID for this environment.\n");
+        wprintf(L"| Can't set %hs to LC_ALL.\n", localeID);
+        wprintf(L"| Check `locale -a` whether specified locale ID is available in this environment.\n");
+        return NULL;
+    }
+    wchar_t** wargs = alloc_wargs(argc, args);
+    setlocale(LC_ALL, old_locale);
+    free(old_locale);
+    return wargs;
+}
+
 // main()
 extern int wmain(int, wchar_t**);
 int main(int argc, char* args[]) {
-    setlocale(LC_ALL, "");
-    wchar_t** wargs = alloc_wargs(argc, args);
+    // Check locale option (-L).
+    char* localeID = "";
+    for (int i = 0; i < argc; i++) {
+        char* arg = args[i];
+        if (strcmp(arg, "-L") == 0) {
+            argc = array_remove((void**)args, i, argc);
+            if (i < argc) {
+                localeID = args[i];
+                argc = array_remove((void**)args, i, argc);
+            }
+        }
+    }
+    wchar_t** wargs = alloc_wargs_with_locale(argc, args, localeID);
     if (wargs == NULL) {
         wprintf(L"Error: Type Conversion Failed (char -> wchar_t).\n");
-        wprintf(L"| Incorrect $LC_CTYPE for command line arguments or this environment.\n");
-        wprintf(L"| - Make sure that $LC_CTYPE is correct for command line encoding.\n");
-        wprintf(L"| - Check `locale -a` whether specified $LC_CTYPE value is available in this environment.\n");
+        wprintf(L"| Incorrect locale for command line arguments.\n");
+        wprintf(L"| - Make sure that current locale is correct for command line encoding.\n");
         for (int i = 0; i < argc; i++) {
             wprintf(L"| args[%d]: ", i);
             char* arg = args[i];
@@ -69,8 +113,7 @@ int main(int argc, char* args[]) {
 //
 int wmain(int argc, wchar_t* args[]) {
     for (int i = 0; i < argc; i++) {
-      wprintf(L"%ls: %d\n",
-          args[i], wcslen(args[i]));
+      wprintf(L"Length[%d]: %d\n", i, wcslen(args[i]));
     }
     return 0;
 }
--- makefile
+++ makefile
@@ -1,11 +1,10 @@
 # GNU makefile for wmain
 
 CC = gcc
-CFLAGS = -std=c99 -Werror -Wall
-#CFLAGS = -std=c99 -Werror -Wall -Wextra
+CFLAGS = -std=gnu99 -Werror -Wall
+#CFLAGS = -std=gnu99 -Werror -Wall -Wextra
 
 TARGET = main.out
-
 OBJS=
 
 all: $(TARGET)
@@ -19,10 +18,14 @@ $(TARGET): $(OBJS) main.o
 ###
 
 SAMPLE = Hello Olá 您好 こんにちは привет "Hello World"
-LC_UTF8=C.UTF-8
-#LC_UTF8=en_US.UTF-8
-run: $(TARGET)
-	LC_ALL=$(LC_UTF8) ./$(TARGET) $(SAMPLE)
+# LC_UTF8 
+#   Specify locale ID that can handle utf-8.
+#   This file is encoded with utf-8,
+#   so $(SAMPLE) is passed in the form of it.
+LC_UTF8 = en_US.UTF-8
+#LC_UTF8 = C.UTF-8
+run_test: $(TARGET)
+	./$(TARGET) -L $(LC_UTF8) $(SAMPLE)
 
 clean:
 	$(RM) *.o
